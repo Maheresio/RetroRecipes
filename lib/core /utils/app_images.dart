8c34a51887f0611147b4edbbc209df61cd766a38
Maheresio <zamalek.545r5@gmail.com>
2025-04-10 13:36:57 +0200

    add required images

---
+++ lib/core /utils/app_images.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+
+  static const String almondImage = "assets/almond.png";
+
+  static const String background1Image = "assets/background 1.png";
+
+  static const String bananaImage = "assets/banana.png";
+
+  static const String breadImage = "assets/bread.png";
+
+  static const String broccoliImage = "assets/broccoli.png";
+
+  static const String cerealImage = "assets/cereal.png";
+
+  static const String chickenSaladImage = "assets/chicken-salad.jpg";
+
+  static const String ckickenSaladImage = "assets/ckicken-salad.webp";
+
+  static const String cornImage = "assets/corn.png";
+
+  static const String gingerImage = "assets/ginger.png";
+
+  static const String grilledChickenSaladImage =
+      "assets/grilled-chicken-salad.jpg";
+
+  static const String mushroomSaladImage = "assets/mushroom salad.jpg";
+
+  static const String oatsImage = "assets/oats.png";
+
+  static const String onionImage = "assets/onion.png";
+
+  static const String potatoImage = "assets/potato.png";
+
+  static const String saldMixImage = "assets/sald-mix.jpg";
+
+  static const String shrimpKaleImage = "assets/Shrimp Kale.jpg";
+
+  static const String thaiSaladImage = "assets/thai salad.jpg";
+}
