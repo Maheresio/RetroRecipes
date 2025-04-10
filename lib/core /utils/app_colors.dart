6e8de432fe43a690805dab50e7fde7a58734ff21
Maheresio <zamalek.545r5@gmail.com>
2025-04-10 13:36:43 +0200

    add the required colors

---
+++ lib/core /utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const recipeAppBackground = Color(0xfffdffff);
+  static const searchBarColor = Color(0xfff6f7fb);
+  static var gradinet = LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [Colors.grey.shade400, Colors.grey.shade200, Colors.white],
+  );
+}
