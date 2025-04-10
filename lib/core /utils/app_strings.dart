92345f0171a7fcaf70465be3c79aebc6d178a2ad
Maheresio <zamalek.545r5@gmail.com>
2025-04-10 13:37:14 +0200

    add required strings

---
+++ lib/core /utils/app_strings.dart
@@ -0,0 +1,7 @@
+class AppStrings {
+  static const kBoardingTitle =
+      'Let\'s cook your own food and adjust your diet';
+  static const kBoardingSubtitle =
+      'Don\'t be confused, Complete your nutritional needs by choosing food here!';
+  static const kGetStarted = 'Get Started ';
+}
