1df98739f8b4051c308a2d0f8dd6ebc8a3600aeb
Maheresio <zamalek.545r5@gmail.com>
2025-04-18 08:14:10 +0200

    add additional app strings for recipe details and nutritional information

--- lib/core /utils/app_strings.dart
+++ lib/core /utils/app_strings.dart
@@ -13,4 +13,30 @@ class AppStrings {
   static const kPopularRecipes = 'Popular Recipes';
   static const kCategories = 'Categories';
 
+
+
+  static const kSeeAll = 'See All';
+
+  static const kRecipeDetails = 'Recipe Details';
+  static const kIngredients = 'Ingredients';
+  static const kInstructions = 'Instructions';
+  static const kOwner = 'Owner';
+  static const kReviews = 'Reviews';
+  static const kAddToFavorite = 'Add to Favorite';
+  static const kAddToShoppingList = 'Add to Shopping List';
+  static const kAddToMealPlan = 'Add to Meal Plan';
+  static const kShare = 'Share';
+
+  
+
+  static const kBowl = 'Bowl';
+  static const kSeeDetails = 'See details';
+
+  static const kCarbs = 'Carbs';
+  static const kFat = 'Fat';
+  static const kProtein = 'Protein';
+  static const kCalories = 'Calories';
+
+
 }
+
